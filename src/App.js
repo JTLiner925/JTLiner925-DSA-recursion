993444e2bfe1c2e3f5a976bf3954b4cb922df8ca
JTLiner925 <[jtliner925@gmail.com]>
2020-07-22 16:26:27 -0500

    worked through first 7 exercises

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-
-  const countSheep = function(num) {
+  const countSheep = function (num) {
     // Base case
-    if (num.length === 0) {
-        return 'All sheep jumped over the fence';
+    if (num === 0) {
+      return "All sheep jumped over the fence";
     }
     // General case
-    return num[0] + countSheep(num.slice(1));
 
-}
+    console.log(`${num}: Another sheep jumps over the fence`);
+    return countSheep(num - 1);
+  };
+  console.log(countSheep(3));
 
-let lst = [3,2,1];
-console.log(countSheep(lst));
+  var powerCalculator = function (a, n) {
+    if (n <= 0) {
+      return 1;
+      console.log("exponent should be >= 0");
+    } else {
+      return (a = a * powerCalculator(a, n - 1));
+    }
+  };
 
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+  console.log(powerCalculator(4, 2));
+  const revString = function (str) {
+    if (str === "") {
+      return "";
+    } else {
+      return (str = revString(str.substr(1)) + str.charAt(0));
+    }
+  };
+  console.log(revString("mom nom"));
+  function tri(n) {
+    if (n <= 1) {
+      return n;
+    } else {
+      return n + tri(n - 1);
+    }
+  }
+
+  console.log(tri(8));
+
+  function multiSplit(xs, delimiters) {
+    if (!delimiters.length) return xs;
+    return xs
+      .split(delimiters[0])
+      .map((x) => multiSplit(x, delimiters.slice(1)));
+  }
+
+  let data = "02/20/2020";
+  let res = multiSplit(data, "/");
+  console.log(res);
+
+  function fibonacci(num) {
+    if (num <= 1) return 1;
+  
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  }
+  console.log(fibonacci(6))
+
+  const factorial = function (num){
+    if(num === 1){
+      return 1;
+    }
+    return num * factorial(num - 1)
+  }
+  console.log(factorial(5))
+
+
+  
+  return <main className="App">{/* content goes here */}</main>;
 }
 
 export default App;
